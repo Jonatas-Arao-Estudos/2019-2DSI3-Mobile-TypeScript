9d7ac7744c274e6304f2af84f7c99cef1ad5e3e1
Jonatas Arão de Carvalho Neves <jonatas12apostolico@hotmail.com>
2019-11-29 22:45:25 +0000

    Ignorem o commit anterior

--- pessoa.ts
+++ pessoa.ts
@@ -1,15 +1,19 @@
 namespace empresa{
     class Pessoa{
+        //Atributos - Características da Classe
          private _nome : string;
          private _idade : number;
          private _cpf : string;
 
+         //Métodos - São as ações da classe
          public getNome() : string{
             return this._nome;
          }
 
          public setNome(nome: string) : void{
+             if(nome != ""){
                 this._nome = nome;
+             }
          }
 
          public getIdade() : number{
@@ -17,7 +21,9 @@ namespace empresa{
          }
 
          public setIdade(idade: number) : void{
+             if(idade != NaN){
                 this._idade = idade;
+             }
          }
 
          public getCpf() : string{
@@ -25,7 +31,9 @@ namespace empresa{
          }
 
          public setCpf(cpf: string) : void{
+             if(cpf != ""){
                 this._cpf = cpf;
+             }
          }
     }
 }
